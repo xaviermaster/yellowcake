ddb1584609b6f97a88a06750badf26131a7b2f25
xaviermaster <xavier.velez3132@gmail.com>
2020-11-24 19:40:10 -0600

    fin

--- content/pages/blog.md
+++ content/pages/blog.md
@@ -1,10 +1,10 @@
 ---
 template: BlogIndex
 slug: blog
-title: Blog Page
+title: Página de Blog
 featuredImage: 'https://ucarecdn.com/b6254577-26bf-4b79-aa9b-f3793e2aebdc/'
-subtitle: This is the blog page subtitle.
+subtitle: Esto es un subtitulo.
 meta:
   description: This is a meta description.
-  title: Blog Page
+  title: Página de Blog
 ---
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -72,7 +72,7 @@ export class Navigation extends Component {
           </Link>
           <div className="Nav--Links">
             <NavLink to="/">Home</NavLink>
-            <NavLink to="/components/">Components</NavLink>
+            <NavLink to="/components/">Componentes</NavLink>
             <div
               className={`Nav--Group ${
                 this.state.activeSubNav === 'posts' ? 'active' : ''
@@ -95,7 +95,7 @@ export class Navigation extends Component {
                 Blog
                 <div className="Nav--GroupLinks">
                   <NavLink to="/blog/" className="Nav--GroupLink">
-                    All Posts
+                    Todos los post
                   </NavLink>
                   {subNav.posts.map((link, index) => (
                     <NavLink
@@ -110,7 +110,7 @@ export class Navigation extends Component {
               </span>
             </div>
             <NavLink to="/default/">Default</NavLink>
-            <NavLink to="/contact/">Contact</NavLink>
+            <NavLink to="/contact/">Contacto</NavLink>
           </div>
           <button
             className="Button-blank Nav--MenuButton"
